526619011451d96a39699a23152e2de5ebdcb224
Henrique Rocha <henryfox2001@hotmail.com>
2023-05-30 12:56:32 -0400

    Finished Learning04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+
+        Assignment assig1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assig1.GetSummary());
+
+        MathAssignment assig2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(assig2.GetSummary());
+        Console.WriteLine(assig2.GetHomeworkList());
+
+        WritingAssignment assig3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(assig3.GetSummary());
+        Console.WriteLine(assig3.GetWritingInformation());
+
     }
 }
