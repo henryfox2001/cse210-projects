130664ac6f1c1921f69b29d2899d54fe47364826
Henrique Rocha <58304860+henryfox2001@users.noreply.github.com>
2023-07-12 01:31:48 -0400

    Delete ScoringRules.cs

--- final/FinalProject/ScoringRules.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public abstract class ScoringRules
-{
-    // Methods
-    public abstract int CalculateScore(int numGuesses, List<string> letters, string word);
-
-    public abstract void ShowScore();
-}
