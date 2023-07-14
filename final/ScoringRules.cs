8e4d9f12db345040f85d5168c669c3b25a96ebff
Henrique Rocha <58304860+henryfox2001@users.noreply.github.com>
2023-07-14 00:45:18 -0400

    Updating ScoringRules.cs

--- final/ScoringRules.cs
+++ final/ScoringRules.cs
@@ -2,6 +2,10 @@ using System;
 
 public abstract class ScoringRules
 {
+    // Attributes
+    protected int score;
+    protected int len;
+    
     // Methods
     public abstract int CalculateScore(int numGuesses, List<string> letters, string word);
 
